375c068bb0a9baacc08dba8ecf4d89ac4c09aa94
kyorohiro <kyorohiro@gmail.com>
2017-02-04 00:17:51 +0900

    ..

--- app/lib/comp_articles.dart
+++ app/lib/comp_articles.dart
@@ -1,5 +1,3 @@
-// Copyright (c) 2017, kyorohiro. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'package:angular2/core.dart';
 import 'package:angular2/router.dart';
@@ -7,15 +5,16 @@ import 'package:cl/config.dart' as config;
 import 'comp_users.dart';
 import 'package:k07me.netbox/netbox.dart';
 import 'comp_article.dart';
-import 'comp_articles.dart';
-
 
 @Component(
-    selector: "my-arts",
-    directives: const [ArticleComponent, ArticlesComponent],
+    selector: "arts-component",
+    directives: const [ArticleComponent],
     template:  """
     <div class="mybody">
-    <arts-component [artNBox]='rootConfig.appNBox.artNBox' [userName]='rootConfig.cookie.userName'></arts-component>
+    <h1>Articles</h1>
+    <div *ngFor='let artInfo of artInfos'>
+        <art-component [artNBox]='artNBox' [artInfo]='artInfo' [isUpdatable]='userName==artInfo.userName'></art-component>
+    </div>
     </div>
   """,
     styles: const[
@@ -27,14 +26,32 @@ import 'comp_articles.dart';
     """,
     ]
 )
-class ArtsPage implements OnInit {
+class ArticlesComponent implements OnInit {
   final RouteParams _routeParams;
-  ArtsPage(this._routeParams);
-  config.AppConfig rootConfig = config.AppConfig.inst;
+  ArticlesComponent(this._routeParams);
 
+  @Input()
+  bool isUpdatable = false;
+
+  @Input()
+  String userName = "";
+
+  @Input()
+  ArtNBox artNBox;
+
+  List<ArtInfoProp> artInfos = [];
 
   ngOnInit() {
     updateConfig();
+    update();
+  }
+
+  update() async {
+    ArtKeyListProp list = await artNBox.findArticle("");
+    for(String key in list.keys) {
+      ArtInfoProp artInfo = await artNBox.getArtFromStringId(key);
+      artInfos.add(artInfo);
+    }
   }
 
   updateConfig(){
